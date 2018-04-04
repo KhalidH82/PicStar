9c4dfddc494fcd2077f8e9b9c12b93a02e7bdffa
Khalid Hosein <khalid.hosein9@gmail.com>
2018-04-03 21:38:18 -0400

    Completed CRU on photos

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ end
 
 gem 'paperclip', '~> 4.2'
 gem 'simple_form'
+gem 'bootstrap-sass', '~> 3.3.5'
+gem 'jquery-rails'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use postgresql as the database for Active Record
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,40 @@
 class PhotosController < ApplicationController
   def index
+  	@photos = Photo.all
   end
 
   def show
+  	@photo = Photo.find(params[:id])
   end
 
   def new
   	@photo = Photo.new
   end
+
+  def create
+  	@photo = Photo.create(post_params)
+  	redirect_to photos_path
+  end
+
+  def edit
+  	@photo = Photo.find(params[:id])
+  end
+
+  def update
+  	@photo = Photo.find(params[:id])
+    @photo.update(post_params)
+    redirect_to(photos_path(@post))
+  end
+
+  private
+
+  def post_params
+
+  params.require(:photo).permit(:image, :location, :description) 
+
+  end
+
+
+
 end
+
